129d2054b5861d8b2df3597717239b98473b6dea
Nasim <nasim.reja.077@gmail.com>
2026-05-03 23:39:58 +0530

    Add implementation of Selection Sort algorithm

---
+++ JS DSA 2026/Sorting/Selection Sort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+
+    for(let i = 0; i < arr.length; i++) {
+
+        let minIndex = i;
+
+        for(let j = i + 1; j < arr.length; j++) {
+
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        [arr[i], arr[minIndex]] =
+        [arr[minIndex], arr[i]];
+    }
+
+    return arr;
+}
+
+console.log(selectionSort([64,25,12,22,11]));
