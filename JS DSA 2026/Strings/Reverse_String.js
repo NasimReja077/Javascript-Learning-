3a5eba4e25579f7761016694fb3125014d095f8b
Nasim <nasim.reja.077@gmail.com>
2026-05-04 23:50:42 +0530

    Rename reverseString function to reverseString1 and add pure recursion implementation for string reversal

--- JS DSA 2026/Strings/Reverse_String.js
+++ JS DSA 2026/Strings/Reverse_String.js
@@ -1,4 +1,4 @@
-function reverseString(text) {
+function reverseString1(text) {
     let chars = text.split("");
 
     let left = 0;
@@ -16,7 +16,7 @@ function reverseString(text) {
     return chars.join("");
 }
 
-console.log(reverseString("hello"));
+console.log(reverseString1("hello"));
 
 
 // Two Pointer Approach
@@ -29,3 +29,50 @@ console.log(reverseString("hello"));
 // Complexity:
 // Time = O(n)
 // Space = O(n)
+
+
+// 2. Pure Recursion Approach
+
+// Good for recursion interviews.
+
+// Code:
+function reverseString2(text) {
+
+    // Base case
+    if (text.length <= 1) {
+        return text;
+    }
+
+    return text[text.length - 1] + reverseString2(text.slice(0, -1));
+}
+
+console.log(reverseString2("hello3"));
+// Complexity:
+// Time = O(n)
+// Space = O(n) (call stack)
+
+// Built-in Method
+
+// function reverseString3(text) {
+//     return text
+//         .split("")
+//         .reverse()
+//         .join("");
+// }
+
+// console.log(reverseString3("hello"));
+
+
+// 4. Manual Loop 
+function reverseString4(text) {
+
+    let reversed = "";
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        reversed += text[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverseString4("hello5"));
